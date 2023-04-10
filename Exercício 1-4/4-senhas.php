1beb012d2211d5e26e8df56b34bf41874208559b
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-10 17:11:10 -0300

    Update 4-senhas.php

--- Exercício 1-4/4-senhas.php
+++ Exercício 1-4/4-senhas.php
@@ -2,17 +2,18 @@
 
 $senha1 = 1234;
 $senha2 = "abcd";
-$resposta = $senha2;
+$senha3 = 4567;
+$resposta = $senha3;
 
 if ($resposta == $senha1) {
-    echo "A senha está certa";
+    echo "A primeira senha está certa";
 }
 
-elseif ($resposta == $senha2) {
-    echo "A senha dois está certa";
+elseif($resposta == $senha2) {
+    echo "A segunda senha está certa";
 }
 
 else {
-    echo "Senha incorreta tente novamente";
+   echo "A senha está errada, tenete novamente";
 }
-
+?>
