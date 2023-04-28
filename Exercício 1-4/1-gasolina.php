75125717ca63bc7f314264f4577bfbe340d5af81
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 13:54:50 -0300

    Update 1-gasolina.php

--- Exercício 1-4/1-gasolina.php
+++ Exercício 1-4/1-gasolina.php
@@ -1,5 +1,7 @@
 <?php
-//Um posto de gasolina vende gasolina a R$ 5,50 o litro. Escreva um programa que calcule o valor total de uma compra de gasolina, com base no número de litros abastecidos pelo cliente. Utilize o operador de multiplicação para calcular o valor total.
+//Um posto de gasolina vende gasolina a R$ 5,50 o litro. Escreva um programa que calcule o 
+//valor total de uma compra de gasolina, com base no número de litros abastecidos pelo cliente.
+//Utilize o operador de multiplicação para calcular o valor total.
 
 
 $n = 5.50;
