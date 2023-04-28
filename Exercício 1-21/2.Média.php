a6ad18a7ac1691343aa8a1598748560bea2fc4ee
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 16:49:38 -0300

    Add files via upload

---
+++ Exercício 1-21/2.Média.php
@@ -0,0 +1,8 @@
+<?php
+$p = readline("Primeira nota: ");
+$d = readline("Segunda nota: ");
+$c = readline("Terceira nota: ");
+
+echo "Nota do bimestre: \n Primeiro bimestre: ". $p ."\n Segundo bimestre: ". $d ."\n Terceira bimestre: ". $c ." \n A soma total é ". $p + $d + $c;
+
+ 
