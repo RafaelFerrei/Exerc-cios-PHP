4042da5bc1458f9742685a1040bae15f5fdf390d
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 13:59:47 -0300

    Update 4-senhas.php

--- Exercício 1-4/4-senhas.php
+++ Exercício 1-4/4-senhas.php
@@ -1,4 +1,13 @@
 <?php
+//Escreva um programa que solicite ao usuário uma senha de acesso.
+//O programa deve permitir o acesso somente se a senha informada
+//pelo usuário for igual a "1234" ou "abcd". Caso contrário, o programa
+//deve imprimir uma mensagem informando que a senha está incorreta e solicitar
+//novamente a senha. Utilize os operadores lógicos (ou e igualdade) para combinar as condições de senha válida.
+
+
+
+
 $arr = array("1234", "abcd");
 
 $senha = readline("Digite a senha:");
