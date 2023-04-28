decc717f0f7dba9bbadf9e9e0b60797a971685a3
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 14:08:41 -0300

    Update and rename Exercício 5-9/9-senha.php to Exercício 1-5/9-senha.php

---
+++ Exercício 1-5/9-senha.php
@@ -0,0 +1,17 @@
+<?php
+//Escreva um programa que solicite ao usuário um número
+//inteiro e verifique se o número é par ou ímpar.
+//O programa deve imprimir uma mensagem informando se o número é par ou ímpar.
+
+
+$senha = 1234;
+$resposta = 1234;
+
+if ($resposta == $senha) {
+    echo "Senha correta!";
+}
+
+else {
+    echo "Senha incorreta!";
+}
+
