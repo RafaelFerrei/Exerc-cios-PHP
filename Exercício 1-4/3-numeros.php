bdf277bdc884e83f9b062ac321e0e5ca964dd5ff
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 13:58:49 -0300

    Update 3-numeros.php

--- Exercício 1-4/3-numeros.php
+++ Exercício 1-4/3-numeros.php
@@ -1,4 +1,10 @@
 <?php
+//Escreva um programa que solicite ao usuário dois números inteiros e verifique
+//se o primeiro número é maior que o segundo número. Se o primeiro número for maior
+//que o segundo número, o programa deve imprimir uma mensagem informando que o primeiro
+//número é maior. Caso contrário, o programa deve imprimir uma mensagem informando
+//que o segundo número é maior ou que ambos os números são iguais.
+
 $n1 = 5;
 $n2 = 10;
 
