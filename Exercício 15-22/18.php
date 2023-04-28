f79d628591f60c87bee0ce51a9167153b0161c7a
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 14:45:53 -0300

    Update and rename Exercício 15-22/18.php to Pasta 1-21/8.php

--- Exercício 15-22/18.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$num = 3;
-
-for ($i=1; $i <=10 ; $i++) { 
-    echo "$num X $i = " . $num * $i . PHP_EOL;
-}
-?>
---
+++ Pasta 1-21/8.php
@@ -0,0 +1,10 @@
+<?php
+//Escreva um script PHP que receba uma string e retorne um array
+//associativo onde as chaves são os caracteres da string e os valores são o número de ocorrências de cada caractere.
+
+$num = 3;
+
+for ($i=1; $i <=10 ; $i++) { 
+    echo "$num X $i = " . $num * $i . PHP_EOL;
+}
+?>
