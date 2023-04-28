094b9d11c99b9ae99adeb4abe4af5f0ff63d4047
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 14:28:23 -0300

    Update and rename 14.ateristico.php to 5.ateristico.php

---
+++ Pasta 1-21/5.ateristico.php
@@ -0,0 +1,16 @@
+<?php
+//Escreva um programa que solicite ao usuário um número inteiro
+//e imprima um padrão formado por asteriscos com base nesse número.
+//Por exemplo, se o usuário informar o número 4, o programa deve imprimir o seguinte padrão:
+
+$n = 5;
+
+for ( $linha = 1; $linha <= n; $linha++) {
+
+    for( $coluna = 1; $coluna <= n; $coluna++) {
+         console.log("*");
+    }
+
+    console.log(" ");
+}
+
