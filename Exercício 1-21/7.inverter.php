47597b5d4c11c5cd6d691fa0730ca34a0730cf73
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 14:36:58 -0300

    Update and rename Exercício 15-22/16.inverter.php to Exercício 1-21/7.inverter.php

--- Exercício 1-21/7.inverter.php
+++ Exercício 1-21/7.inverter.php
@@ -1,4 +1,6 @@
 <?php
+//Escreva um script PHP que inverta a ordem dos elementos de um array sem usar funções nativas do PHP.
+
 $input  = array("Shazam", 4.0, array("Pepino", "amarelo"));
 $reservado = array_reverse($input);
 $preservado = array_reverse($input, true);
