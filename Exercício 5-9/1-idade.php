b1ce12bf6045efab75733c23c77eba722dd03a85
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 14:01:11 -0300

    Update and rename 5-idade.php to 1-idade.php

---
+++ Exercício 5-9/1-idade.php
@@ -0,0 +1,12 @@
+<?php
+//Escreva um programa que solicite ao usuário sua idade. O programa deve imprimir uma mensagem informando se o usuário é maior ou menor de idade.
+
+$idade = 17;
+
+if ($idade >= 18) {
+    echo "Ele é maior de idade";
+}
+
+else {
+    echo "Ele ainda é menor de idade";
+}
