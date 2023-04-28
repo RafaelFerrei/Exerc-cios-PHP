c60957bc9bc446cccffbe982623fecaaeddf1730
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 14:06:30 -0300

    Update and rename Exercício 5-9/8-parouimpar.php to Exercício 1-5/4-parouimpar.php

---
+++ Exercício 1-5/4-parouimpar.php
@@ -0,0 +1,15 @@
+<?php
+//Escreva um programa que solicite ao usuário um nome de usuário e uma senha.
+//O programa deve permitir o acesso somente se o nome de usuário
+//for "admin" e a senha for "1234". Caso contrário, o programa deve
+//imprimir uma mensagem informando que o login está incorreto
+//e solicitar novamente o nome de usuário e a senha.
+
+
+$valor = 3;
+
+if($valor % 2 == 0) {
+  echo "Numero Par";
+} else {
+  echo "Numero Impar";
+} ?>
--- Exercício 5-9/8-parouimpar.php
+++
@@ -1,8 +0,0 @@
-<?php
-$valor = 3;
-
-if($valor % 2 == 0) {
-  echo "Numero Par";
-} else {
-  echo "Numero Impar";
-} ?>
