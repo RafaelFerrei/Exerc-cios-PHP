4157b58acb55fe64975ebcdedda3264769667ace
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-04-28 13:55:57 -0300

    Update 2-trabalhador.php

--- Exercício 1-4/2-trabalhador.php
+++ Exercício 1-4/2-trabalhador.php
@@ -1,4 +1,11 @@
 <?php
+
+//Um trabalhador recebe um salário base de R$ 2.500 por mês.
+//Além disso, ele recebe uma comissão de 5% sobre o valor total de vendas realizadas no mês.
+//Escreva um programa que calcule o salário final do trabalhador,
+//com base no número de vendas realizadas e no valor total das vendas.
+//Utilize o operador de multiplicação e atribuição para calcular a comissão e atualizar o salário base.
+
 $x = 2500;
 $z = 1500;
 
