694cf13a7d00bab31e46e9363d020e56869de5b0
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-05-02 00:48:12 -0300

    Add files via upload

---
+++ Exercício 1-21/13.primo.php
@@ -0,0 +1,10 @@
+<?php
+  $n = 4;
+
+  
+  for($count = 2; $count <= $n; $count++)
+   if($n % $count == 0){
+    echo "Multiplo de $count";
+ 
+   }
+  
---
+++ Exercício 1-21/?11.maiornumero.php
@@ -0,0 +1,7 @@
+<?php
+$n = array(1,2,3,4,5,6);
+foreach($n as $m){
+return($n = 0);
+return($m = 10);
+ }
+ echo"O maior número é ". $n .", e o menor é ". $m;
