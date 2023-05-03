f299ac043ca8f00a005b8370e699e4fb66b20f01
RafaelFerrei <129790576+RafaelFerrei@users.noreply.github.com>
2023-05-03 17:22:32 -0300

    Update 1-idade.php

--- Exercício 1-5/1-idade.php
+++ Exercício 1-5/1-idade.php
@@ -1,7 +1,7 @@
 <?php
 //Escreva um programa que solicite ao usuário sua idade. O programa deve imprimir uma mensagem informando se o usuário é maior ou menor de idade.
 
-$idade = 17;
+$idade = readline("Digite a sua idade: ");
 
 if ($idade >= 18) {
     echo "Ele é maior de idade";
